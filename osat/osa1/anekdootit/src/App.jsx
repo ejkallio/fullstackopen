5f3859cb99d6a1c2a0112291c4314d998311df98
ejkallio <eliasjaakko13@gmail.com>
2023-09-21 13:25:41 +0300

    tasks 1.12-1.14 done

---
+++ osat/osa1/anekdootit/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  const { handleClick, text } = props
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const getRandomInt = (max) => (
+  Math.floor(Math.random() * max)
+)
+
+const Display = ({text, points}) => (
+    <div>
+      {text} <br />
+      has {points} votes
+    </div>
+)
+
+const Header = ({text}) => (
+  <h1>{text}</h1>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleSelect = () => {
+    setSelected(getRandomInt(anecdotes.length))
+  }
+
+  const handlePoints = () => {
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
+  }
+
+  const highestVotes = points.indexOf(Math.max(...points));
+
+  return (
+    <div>
+      <Header text='Anecdote of the day' />
+      <Display text={anecdotes[selected]} points={points[selected]} />
+      <Button handleClick={handlePoints} text='vote' />
+      <Button handleClick={handleSelect} text='next anecdote' />
+      <Header text='Anecdote with the most votes' />
+      <Display text={anecdotes[highestVotes]} points={points[highestVotes]} />
+    </div>
+  )
+}
+
+export default App
