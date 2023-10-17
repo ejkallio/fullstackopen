10c55906964bce299d4c80d3b5eca73a76872fcd
ejkallio <eliasjaakko13@gmail.com>
2023-10-17 13:51:39 +0300

    exercises pushed to git

---
+++ osat/harjoitukset/part3helloworld/index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+  const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+app.use(express.json())
+app.use(requestLogger)
+app.use(unknownEndpoint)
+app.use(cors())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    console.log(id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body
+    console.log(note)
+    res.json(note)
+})
+
+const PORT = process.nextTick.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
