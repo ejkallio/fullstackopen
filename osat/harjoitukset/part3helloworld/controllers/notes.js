0731a66d989bb694003bafc1a818d268ab87cbf6
ejkallio <eliasjaakko13@gmail.com>
2025-05-26 13:03:27 +0300

    testiharjoitukset tehty

--- osat/harjoitukset/part3helloworld/controllers/notes.js
+++ osat/harjoitukset/part3helloworld/controllers/notes.js
@@ -1,48 +1,35 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find({})
+  res.json(notes)
 })
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then(note => {
-      if (note) {
-        res.json(note)
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(error => next(error))
-})
-
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-notesRouter.post('/', (req, res, next) => {
-  const body = req.body
-
-  if (body.content === undefined) {
-    return res.status(400).json({ error: 'content missing' })
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
   }
+})
+
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id)
+  res.status(204).end()
+})
+
+notesRouter.post('/', async (req, res) => {
+  const body = req.body
 
   const note = new Note ({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    res.json(savedNote)
-  })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  res.status(201).json(savedNote)
 })
 
 notesRouter.put('/:id', (req, res, next) => {
