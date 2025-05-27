86275c3281fdd3db11a769e032fd733480622bc3
ejkallio <eliasjaakko13@gmail.com>
2025-05-27 13:10:27 +0300

    osan 4 notes-harjoitukset tehty

--- osat/harjoitukset/part3helloworld/controllers/notes.js
+++ osat/harjoitukset/part3helloworld/controllers/notes.js
@@ -1,8 +1,20 @@
 const notesRouter = require('express').Router()
+const { request } = require('../app')
 const Note = require('../models/note')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 notesRouter.get('/', async (req, res) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
   res.json(notes)
 })
 
@@ -20,29 +32,40 @@ notesRouter.delete('/:id', async (req, res) => {
   res.status(204).end()
 })
 
-notesRouter.post('/', async (req, res) => {
-  const body = req.body
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
-  const note = new Note ({
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+
+  const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
-  res.status(201).json(savedNote)
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.status(201).json(savedNote)
 })
 
-notesRouter.put('/:id', (req, res, next) => {
+notesRouter.put('/:id', async (req, res) => {
   const { content, important } = req.body
 
-  Note.findByIdAndUpdate(
+  const updatedNote = await Note.findByIdAndUpdate(
     req.params.id,
     { content, important },
     { new: true, runValidators: true, context: 'query' })
-    .then(updatedNote => {
-      res.json(updatedNote)
-    })
-    .catch(error => next(error))
+
+  res.json(updatedNote)
 })
 
 module.exports = notesRouter
