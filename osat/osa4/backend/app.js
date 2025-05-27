4c7af5d2ff3d76daee1133f125d312635f8521f8
ejkallio <eliasjaakko13@gmail.com>
2025-05-27 15:00:14 +0300

    tehtävä 4.15 tehty

--- osat/osa4/backend/app.js
+++ osat/osa4/backend/app.js
@@ -2,7 +2,10 @@ require('express-async-errors')
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -15,11 +18,18 @@ mongoose.connect(config.MONGODB_URI)
     logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.messasge)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', userRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
