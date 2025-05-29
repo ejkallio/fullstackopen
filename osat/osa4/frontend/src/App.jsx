5f1c2b7099058c2b43e7d4a240849d1ff86ca83d
ejkallio <eliasjaakko13@gmail.com>
2025-05-29 15:54:15 +0300

    tehtävä 5.10 tehty

--- osat/osa4/frontend/src/App.jsx
+++ osat/osa4/frontend/src/App.jsx
@@ -191,6 +191,8 @@ const App = () => {
 
 
       {blogs
+        .slice()
+        .sort((a,b) => b.likes - a.likes)
         .map(blog =>
         <Blog key={blog.id} blog={blog} onLike={handleLike} />
       )}
